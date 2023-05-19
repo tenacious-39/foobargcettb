85aa76e0418ed43e0211e7627674dfebad184ede
Kritidipta Ghosh <86186712+tenacious-39@users.noreply.github.com>
2023-05-20 00:08:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -126,9 +126,9 @@ app.post("/updateHandles", function (req, res) {
 }
 );
 
-app.get("/", function (req, res) {
-    res.send("<h1>Hello World</h1>");
-})
+// app.get("/", function (req, res) {
+//     res.send("<h1>Hello World</h1>");
+// })
 
 if (process.env.NODE_ENV == "production") {
     const path = require('path');
