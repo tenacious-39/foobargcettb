90519a99b8f7f379dd6b14af1a4b5bdc947808bf
Kritidipta <kritidiptaghosh30900@gmail.com>
2023-05-20 20:13:33 +0530

    fixing homepage v2

--- app.js
+++ app.js
@@ -136,11 +136,6 @@ if (process.env.NODE_ENV == "production") {
         app.use(express.static(path.resolve(__dirname, 'client', 'build')));
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-
-    app.get('/auth/success', function (req, res) {
-        app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
 }
 
 app.listen(PORT, function () {
